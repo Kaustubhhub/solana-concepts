0b63068c4e3450a4a6f790314c752b8d6cd80672
kaustubh <kaustubhkumbhare02@gmail.com>
2025-06-25 10:26:03 +0000

    reading account

---
+++ reading_from_nw/dist/fetch_wallet_accounts.js
@@ -0,0 +1,22 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const web3_js_1 = require("@solana/web3.js");
+const readingFromNetwork = () => __awaiter(void 0, void 0, void 0, function* () {
+    const keypair = web3_js_1.Keypair.generate();
+    const publicKey = keypair.publicKey;
+    const connection = new web3_js_1.Connection("http://127.0.0.1:8899", "confirmed");
+    const signature = yield connection.requestAirdrop(publicKey, web3_js_1.LAMPORTS_PER_SOL);
+    yield connection.confirmTransaction(signature, "confirmed");
+    const accountInfo = yield connection.getAccountInfo(publicKey);
+    console.log(JSON.stringify(accountInfo, null, 2));
+});
+readingFromNetwork();
---
+++ reading_from_nw/src/fetch_wallet_accounts.ts
@@ -0,0 +1,22 @@
+import {Connection, Keypair, LAMPORTS_PER_SOL} from '@solana/web3.js'
+
+const keypair = Keypair.generate();
+const publicKey = keypair.publicKey;
+
+const fetchWalletAccount = async() =>{
+    
+    const connection = new Connection("http://127.0.0.1:8899","confirmed");
+
+    const signature = await connection.requestAirdrop(publicKey,LAMPORTS_PER_SOL);
+
+    await connection.confirmTransaction(signature,"confirmed");
+
+    const accountInfo = await connection.getAccountInfo(publicKey)
+    console.log(JSON.stringify(accountInfo, null, 2));
+}
+
+const fetchTokenAccount = async() =>{
+
+}
+
+fetchWalletAccount();
